7c697c38673fa8a3f532f34294fb0538473cf23f
mpicazz-247 <mariopicazzo2004@gmail.com>
2025-12-06 22:03:17 -0600

    timerSettings.java class functional code written. stores initial variable values repetition, breakTime, and studyTime.

--- src/main/java/com/example/studytool/timerSettings.java
+++ src/main/java/com/example/studytool/timerSettings.java
@@ -1,4 +1,23 @@
 package com.example.studytool;
 
 public class timerSettings {
+    private int repetition;
+    private int breakTime;
+    private int studyTime;
+
+    public timerSettings(int repetition, int breakTime, int studyTime){
+        this.repetition = repetition;
+        this.breakTime = breakTime;
+        this.studyTime = studyTime;
+    }
+
+    public int getRepetition(){
+       return repetition;
+    }
+    public int getBreakTime(){
+        return breakTime;
+    }
+    public int getStudyTime(){
+        return studyTime;
+    }
 }
