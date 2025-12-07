ea424a5d9639444a08c0f2b74ae804c1ec28da5c
mpicazz-247 <mariopicazzo2004@gmail.com>
2025-12-06 22:07:05 -0600

    added comments in timerSettings.java describing functions.

--- src/main/java/com/example/studytool/timerSettings.java
+++ src/main/java/com/example/studytool/timerSettings.java
@@ -1,18 +1,18 @@
 package com.example.studytool;
 
 public class timerSettings {
-    private int repetition;
-    private int breakTime;
-    private int studyTime;
-
+    private int repetition; //number of times pomodoro timer repeats itself
+    private int breakTime; //length of breaks in minutes
+    private int studyTime; //length of study intervals in minutes
+    //constructor to allow settings objects to be created later in other classes
     public timerSettings(int repetition, int breakTime, int studyTime){
         this.repetition = repetition;
         this.breakTime = breakTime;
         this.studyTime = studyTime;
     }
-
+//getters to allow other classes to read these values
     public int getRepetition(){
-       return repetition;
+        return repetition;
     }
     public int getBreakTime(){
         return breakTime;
