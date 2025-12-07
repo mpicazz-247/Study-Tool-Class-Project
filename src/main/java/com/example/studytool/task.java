5c1a563152808af1d24fa8a6403415e723e11acc
mpicazz-247 <mariopicazzo2004@gmail.com>
2025-12-07 02:23:00 -0600

    completed task.java class with comments. stores task title and checkmark status

--- src/main/java/com/example/studytool/task.java
+++ src/main/java/com/example/studytool/task.java
@@ -1,4 +1,22 @@
 package com.example.studytool;
 
 public class task {
+    private String title; //task name
+    private boolean completed; //task status
+    //constructor to make task objects
+    public task(String title) {
+        this.title = title;
+        this.completed = completed;
+    }
+    //getters and setters.
+    public String getTitle() {
+        return title;
+    }
+    public boolean getCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean value){
+        completed = value;
+    } //setter for completed to turn on and off
+
 }
