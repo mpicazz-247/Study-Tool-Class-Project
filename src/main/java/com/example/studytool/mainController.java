66dcd39156e723958d9d05ff03c1bd206f67b378
mpicazz-247 <mariopicazzo2004@gmail.com>
2025-12-07 19:01:12 -0600

    progress in writing mainController.java, added UI variables. realized the pomodoroTimer will only return seconds, not minutes. will add formatTime getter in pomodoroTimer class

--- src/main/java/com/example/studytool/mainController.java
+++ src/main/java/com/example/studytool/mainController.java
@@ -1,5 +1,38 @@
 package com.example.studytool;
 
-public class mainController {
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+
+public class mainController {
+    // pomodoro timer UI
+    @FXML private Label timerLabel;
+    @FXML private Button startStopButton;
+    @FXML private Button skipButton;
+    @FXML private Button resetButton;
+    @FXML private RadioButton mode25_5;
+    @FXML private RadioButton mode50_10;
+    @FXML private ComboBox<Integer> repetetionSelector;
+    // to-do list UI
+    @FXML private Button addButton;
+    @FXML private Button toggleButton;
+    @FXML private Button removeButton;
+    @FXML private TextField taskInput;
+    @FXML private ListView<task> taskListView;
+    //data
+    private ObservableList<task> tasks = FXCollections.observableArrayList();
+    private timerSettings settings;
+    private pomodoroTimer timer;
+
+    @FXML
+    public void intialize(){
+        //setting the default values of the timer to
+        // 1 repetition, 5 minutes of break, and 25 minutes of studying
+        settings = new timerSettings(1,5,25);
+        timer = new pomodoroTimer(settings, () -> {
+            timerLabel.setText(timer.getRemainingSeconds()/60+ " (" + timer.getPhase() + ")");
+        });
+    }
 }
--- src/main/java/com/example/studytool/toDoController.java
+++
@@ -1,5 +0,0 @@
-package com.example.studytool;
-
-public class toDoController {
-
-}
