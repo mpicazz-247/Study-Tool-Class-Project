85a23746d772bba9a0c2bb7ade8c5a1ca294a2d0
mpicazz-247 <mariopicazzo2004@gmail.com>
2025-12-07 20:11:48 -0600

    main JavaFX application finished.

--- src/main/java/com/example/studytool/main.java
+++ src/main/java/com/example/studytool/main.java
@@ -1,9 +1,24 @@
 package com.example.studytool;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
-public class main {
-    public static void main(String[] args) {
+public class main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/studytool/main.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        primaryStage.setTitle("Pomodoro Study Tool");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
